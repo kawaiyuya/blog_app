220b34faf95f9ccd0efce9b891da56250b8f5f05
kawaiyuya <hi.nose4n@gmail.com>
2020-04-19 09:11:43 +0900

    新規投稿作成、投稿編集着手

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -3,24 +3,13 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 
-/**
- * Posts Controller
- *
- * @property \App\Model\Table\PostsTable $Posts
- *
- * @method \App\Model\Entity\Post[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
- */
+
 class PostsController extends AppController
 {
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|null
-     */
+
     public function index()
     {
         $posts = $this->Posts->find('all');
-        // $this->set('posts', $posts);
         $this->set(compact('posts'));
 
         // $this->paginate = [
@@ -31,13 +20,7 @@ class PostsController extends AppController
         // $this->set(compact('posts'));
     }
 
-    /**
-     * View method
-     *
-     * @param string|null $id Post id.
-     * @return \Cake\Http\Response|null
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
+
     public function view($id = null)
     {
         $post = $this->Posts->get($id, [
@@ -47,11 +30,6 @@ class PostsController extends AppController
         $this->set('post', $post);
     }
 
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
-     */
     public function add()
     {
         $post = $this->Posts->newEntity();
@@ -68,13 +46,6 @@ class PostsController extends AppController
         $this->set(compact('post', 'users'));
     }
 
-    /**
-     * Edit method
-     *
-     * @param string|null $id Post id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
     public function edit($id = null)
     {
         $post = $this->Posts->get($id, [
@@ -93,13 +64,6 @@ class PostsController extends AppController
         $this->set(compact('post', 'users'));
     }
 
-    /**
-     * Delete method
-     *
-     * @param string|null $id Post id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
     public function delete($id = null)
     {
         $this->request->allowMethod(['post', 'delete']);
--- src/Template/Posts/edit.ctp
+++ src/Template/Posts/edit.ctp
@@ -4,7 +4,7 @@
  * @var \App\Model\Entity\Post $post
  */
 ?>
-<nav class="large-3 medium-4 columns" id="actions-sidebar">
+<nav>
     <ul class="side-nav">
         <li class="heading"><?= __('Actions') ?></li>
         <li><?= $this->Form->postLink(
